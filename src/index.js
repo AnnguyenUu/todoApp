c48d98da3a70f3e977c582e9c4ec072e01a6e6a5
huybui <annguyen@aitt.vn>
2020-08-04 22:26:42 +0700

    title for react site

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDom from 'react-dom';
 import App from './App.js'
 
 class TodoList extends React.Component {
+	componentDidMount(){
+		document.title = "To Do App"
+	  }
 	render() {
 		return(
 		<App />
